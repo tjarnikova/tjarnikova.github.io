0916b06fc83af6f4faac29b4a7b5c64d694c5810
Tereza Jarnikova <tjarnik@gmail.com>
2023-04-16 17:09:55 +0100

    lol

--- Divis/script.js
+++ Divis/script.js
@@ -1,9 +1,16 @@
 
 let quotes=[
 
-            'vole',
-            'vole2',
-            'vole3',
+            'Básníkovi rádi a rychle odpouštíme neblahý život pro dobré dílo, nikdy však mizerné dílo, byť by býval světcem.',
+            'Cítíme, že tajemství existuje a že existuje i zázrak. Další dva výrazy úporně se vzpěčující jakékoli definici a výměru. Definicí čtverce je a2 a krychle a3. Jenže z definic co normální lidé, nezabývající se právě matematikou, musíme vzápětí vystoupit, ne-li odtud prchat.',
+            'Inspirace, co to je? Je to od slova inspiro, nadechnout se. Já nerad to slovo frekventuju, ale nemám jiné po ruce. Je to zázrak: mně se to za 43 let básnickému principu stalo pětkrát, nanejvýš šestkrát. Všechny ty stavy si pamatuju, tak pronikavé jsou ty zážitky...',
+            'Nedat si ze zásady s ničím práci; simplifikovat notoricky a debilně — podstata fašismu. Absolutní nevědomost, rochnění se v ní, nechtít vědět, že nevím, pýcha na tento stav, konec.',
+            'Jen neštěstí nás přivádí k sobě samým.',
+            'Každý člověk má všechno. I svého génia, i kolosální tupost, i schopnost rozhořčení, shovívavost i rouhačství a třeba v skrytu nošené ponětí o svatosti, svůj um a zkum a svoje nadání, i lásku k hudbě, i naprostou amúzičnost, i svou radost i své tajné hoře, domnívám se, že co do vloh a výbavy není lidí privilegovaných.',
+            'To nevím, ale zato vím, že jsem vynikající český básník.',
+            'Považuji za nežádoucí zkrat, dokonce za svého druhu příkoří, aby po umělci zbylo jen a jen dílo a současně nebyl vydán počet i o jeho soukromém utrpení, kterým je nejen vykupoval, ale bez něhož by prostě nebyl ani občanem, ani otcem, doslova nikým.',
+            'Talent bez statečnosti je jak dort s polevou z hoven.',
+            'Mám na světě jen jediného přítele, totiž sebe sama.',
 
 ];
 
